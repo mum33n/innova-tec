f47423c931b07d38159e77ef532d9727117aa737
mum33n <azeezmumeen6@gmail.com>
2022-01-30 04:07:54 -0800

    commit

---
+++ index.14b17384.js
@@ -0,0 +1,2 @@
+var wow=new WOW;wow.init();const toggler=document.getElementById("mobile-menu");toggler.addEventListener("click",(function(){toggler.classList.toggle("is-active")}));const options={strings:["INNOVA TECHNOLOGY"],typeSpeed:100},typed=new Typed(".typing",options);
+//# sourceMappingURL=index.14b17384.js.map
